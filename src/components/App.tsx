1abbafaed4eb08fe1b5c655d4621af7b643fef59
Shubham Lad <shubhamlad1001@gmail.com>
2021-10-19 00:11:05 +0530

    app scaffold done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Grid } from "semantic-ui-react";
 
-function App() {
+import "./App.css";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+import SidePanel from "./SidePanel/SidePanel";
+
+const App = () => {
   return (
-    <div>Slack-Clone</div>
+    <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
   );
-}
+};
 
 export default App;
